cbb23ae2ddbf456e4882fb4c9a08c6e167b39cc0
stafamucalju <ximuboo@gmail.com>
2019-07-14 21:40:18 +0200

    prepare for recycler view adapter

--- app/src/main/java/psychegrammer/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/psychegrammer/example/coderswag/Controller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import psychegrammer.example.coderswag.Adapter.CategoryAdapter
 import psychegrammer.example.coderswag.Model.Category
@@ -19,7 +20,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         adapter = CategoryAdapter(this, DataService.categories)
-
         categoryListView.adapter = adapter
+        
+
     }
 }
