180a11112812a4a17d78fb42dbeda8605951ef0b
stafamucalju <ximuboo@gmail.com>
2019-07-14 20:49:55 +0200

    lesson 4

--- app/src/main/java/psychegrammer/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/psychegrammer/example/coderswag/Controller/MainActivity.kt
@@ -5,20 +5,20 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
+import psychegrammer.example.coderswag.Adapter.CategoryAdapter
 import psychegrammer.example.coderswag.Model.Category
 import psychegrammer.example.coderswag.R
 import psychegrammer.example.coderswag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
