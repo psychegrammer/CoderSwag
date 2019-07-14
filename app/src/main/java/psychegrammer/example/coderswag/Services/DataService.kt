2800d7dbf915bc4ef273889aef6423a1ba43d39b
stafamucalju <ximuboo@gmail.com>
2019-07-15 00:45:46 +0200

    lesson 6

--- app/src/main/java/psychegrammer/example/coderswag/Services/DataService.kt
+++ app/src/main/java/psychegrammer/example/coderswag/Services/DataService.kt
@@ -20,17 +20,28 @@ object DataService {
     )
 
     val hoodies = listOf(
-        Product("Devslopes Hoodie Gray", "$28", "hoodie01"),
-        Product("Devslopes Hoodie Red", "$30", "hoodie02"),
-        Product("Devslopes Gray Hoodie", "$28", "hoodie03"),
-        Product("Devslopes Black Hoodie", "$22", "hoodie04")
+        Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+        Product("Devslopes Hoodie Red", "$30", "hoodie2"),
+        Product("Devslopes Gray Hoodie", "$28", "hoodie3"),
+        Product("Devslopes Black Hoodie", "$22", "hoodie4")
     )
 
     val shirts = listOf(
-        Product("Devslopes Shirt Black","$18", "shirt01"),
-        Product("Devslopes Badge Light Gray","$20", "shirt02"),
-        Product("Devslopes Logo Shirt Red","$21", "shirt03"),
-        Product("Devslopes Hustle","$22", "shirt04"),
-        Product("Kickflip Studios","$24", "shirt05")
+        Product("Devslopes Shirt Black","$18", "shirt1"),
+        Product("Devslopes Badge Light Gray","$20", "shirt2"),
+        Product("Devslopes Logo Shirt Red","$21", "shirt3"),
+        Product("Devslopes Hustle","$22", "shirt4"),
+        Product("Kickflip Studios","$24", "shirt5")
     )
+
+    val digitalGoods = listOf<Product>()
+
+    fun getProducts(category: String) : List<Product> {
+        return when (category) {
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGoods
+        }
+    }
 }
